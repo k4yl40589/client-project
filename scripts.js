e4c8f2093b5c2bafda5279cbba6f28bdd89cbdb9
k4yl40589 <k4yl40589@gmail.com>
2025-05-15 19:56:30 -0500

    client-project

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function toggleNav() {
+    let elements = document.getElementsByClassName('toggle-nav');
+    for(let i = 0; i < elements.length; i++){
+    elements[i].classList.toggle('show');
+    }
+}
+document.addEventListener("DOMContentLoaded", function(){
+	document.querySelector('.toggle-nav .hamburger').addEventListener('click', function(){
+		document.querySelector('.toggle-nav').classList.toggle('show');
+	});
+});
